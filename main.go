3dd8f9cf013797c71687dde338680b9c712aee5f
josiah <josiah@appstack.nyc>
2024-11-09 11:02:55 -0500

    packge up some code i use alot

---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"sync"
+	"time"
+
+	"github.com/google/uuid"
+	sqlite "github.com/mattn/go-sqlite3"
+)
+
+/*
+init registers the sqlite3 driver with the extended functions
+
+	This MUST be run first before any other database operations
+*/
+func init() {
+	log.Println("Extending sqlite3 driver...")
+
+	var CONNECT_ONCE sync.Once
+
+	CONNECT_ONCE.Do(func() {
+		sql.Register("sqlite3_extended", &sqlite.SQLiteDriver{
+			ConnectHook: func(conn *sqlite.SQLiteConn) error {
+				if err := conn.RegisterFunc("uuid", newUUID, false); err != nil {
+					return err
+				}
+
+				if err := conn.RegisterFunc("current_timestamp", currentTimestamp, false); err != nil {
+					return err
+				}
+
+				return nil
+			},
+		})
+	})
+	log.Println("Extended sqlite3 driver successfully")
+}
+
+func currentTimestamp() string { return time.Now().Format("2006-01-02 15:04:05") }
+
+func newUUID() string { return uuid.New().String() }
+
+/*
+Connects to the database and returns the connection.
+
+	Errors if the location is empty || the connection fails.
+*/
+func ConnectToExtendedSqliteDatabase(dbLocation string) (*sql.DB, error) {
+	if dbLocation == "" {
+		return nil, fmt.Errorf("Database location cannot be empty")
+	}
+
+	log.Println("Connecting to database...")
+
+	connection, err := sql.Open("sqlite3_extended", dbLocation)
+
+	if connection.Ping() == nil || err != nil {
+		return nil, fmt.Errorf("Failed to connect the database: %s", dbLocation)
+	}
+
+	log.Println("Database connected successfully")
+
+	return connection, nil
+}
---
+++ main_test.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestConnectToDatabase(t *testing.T) {
+	db_conn, err := ConnectToExtendedSqliteDatabase("file::memory:?cache=shared")
+	assert.NoError(t, err)
+	assert.NotNil(t, db_conn)
+}
